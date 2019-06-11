8640eceb2c66f63e0956e133969e95775e18a578
Dan Austin <djadevelopment@gmail.com>
2019-06-11 20:01:35 +0100

    Add views for question items

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,19 @@
+# pylint: disable=no-member
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ''.join([f"<li>{q.question_text}</li>" for q in latest_question_list])
+    return HttpResponse(f"Hello, world. You're at the polls index.\n {output}")
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results for question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
