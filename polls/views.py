959b3a3e81a1912593c0bcf95688be936fbea0a0
Dan Austin <djadevelopment@gmail.com>
2019-06-11 20:21:07 +0100

    Remove unused import

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,6 @@
 # pylint: disable=no-member
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 from .models import Question
 # Create your views here.
 
