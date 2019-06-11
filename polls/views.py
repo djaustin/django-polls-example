f6f79311442a2e96766f91512046c80eb36f6411
Dan Austin <djadevelopment@gmail.com>
2019-06-11 20:16:28 +0100

    add template for question index

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,25 @@
 # pylint: disable=no-member
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 # Create your views here.
 
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ''.join([f"<li>{q.question_text}</li>" for q in latest_question_list])
-    return HttpResponse(f"Hello, world. You're at the polls index.\n {output}")
+    template = loader.get_template('index.html')
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context))
+
 
 def detail(request, question_id):
     return HttpResponse(f"You're looking at question {question_id}")
 
+
 def results(request, question_id):
     return HttpResponse(f"You're looking at the results for question {question_id}")
+
 
 def vote(request, question_id):
     return HttpResponse(f"You're voting on question {question_id}")
